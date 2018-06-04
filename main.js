2eac00f44c865b3a4d00961e0cbe3c267ffe8273
Push <crashjohnson@gmail.com>
2018-06-04 02:39:49 -0500

    Got jumbotron carousel to populate the 10 most recent trending gifs as individual slides

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
 let apiKey = 'zVIVp4ETXeNFTJGDlxDKhaodyQgovHBA';
+let trendRelated;
+
+
+
+function getTrending() {
+    $.ajax({
+        url: 'http://api.giphy.com/v1/gifs/trending?api_key='+apiKey+'&limit=10',
+        method: 'GET'
+    }).then(function(trendingObject){
+        let tData = trendingObject.data;
+        
+        $(tData).each(function(i){
+            let title = this.title;
+            let shortTitle = title.split(' ').slice(0,3).join(' ');
+            let trendSearch = title.split(' ').slice(0,3).join('_');
+             $('.carousel-inner').append(`
+                <div id ="slide${i}" class="carousel-item">
+                    <div class="row carouselRow">
+                        <div class="col-md-4">
+                            <h3 class="imgTitle">${this.title}</h3>
+                            <img class="d-block w-100" src="${this.images.original.url}" alt="slide ${i+1}">
+                            <p class="float-left imgRating">Rating: ${this.rating}</p>
+                            <p class="float-right imgLink"><a href="${this.bitly_url}" target="blank">Visit Page</a></p>
+                        </div>
+                        
+                        <div class="col-md-8">
+                        <h4 class="realtedTitle text-right">Gifs related to: ${shortTitle}</h4>
+                        </div>
+                    </div>
+                </div>
+            `);
+            $('#slide0').addClass('active');
+
+        });
+
+    })
+}
+
+getTrending();
