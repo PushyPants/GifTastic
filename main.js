651bc83bdbfca9a2fe33a173150388b5ffe49e38
Push <crashjohnson@gmail.com>
2018-06-04 03:17:56 -0500

    got each of the related objects to console log 6 serach results related to the first 3 words of thier title

--- main.js
+++ main.js
@@ -14,23 +14,39 @@ function getTrending() {
             let title = this.title;
             let shortTitle = title.split(' ').slice(0,3).join(' ');
             let trendSearch = title.split(' ').slice(0,3).join('_');
-             $('.carousel-inner').append(`
-                <div id ="slide${i}" class="carousel-item">
-                    <div class="row carouselRow">
-                        <div class="col-md-4">
-                            <h3 class="imgTitle">${this.title}</h3>
-                            <img class="d-block w-100" src="${this.images.original.url}" alt="slide ${i+1}">
-                            <p class="float-left imgRating">Rating: ${this.rating}</p>
-                            <p class="float-right imgLink"><a href="${this.bitly_url}" target="blank">Visit Page</a></p>
-                        </div>
-                        
-                        <div class="col-md-8">
-                        <h4 class="realtedTitle text-right">Gifs related to: ${shortTitle}</h4>
-                        </div>
-                    </div>
-                </div>
+            let relatedGifs;
+
+            
+            $('.carousel-inner').append(`
+            <div id ="slide${i}" class="carousel-item">
+            <div class="row carouselRow">
+            <div class="col-md-4">
+            <h3 class="imgTitle">${this.title}</h3>
+            <img class="d-block w-100" src="${this.images.original.url}" alt="slide ${i+1}">
+            <p class="float-left imgRating">Rating: ${this.rating}</p>
+            <p class="float-right imgLink"><a href="${this.bitly_url}" target="blank">Visit Page</a></p>
+            </div>
+            
+            <div class="col-md-8">
+            <h4 class="relatedTitle text-right">Gifs related to: ${shortTitle}</h4>
+            </div>
+            </div>
+            </div>
             `);
             $('#slide0').addClass('active');
+            
+            $.ajax({
+                url: 'http://api.giphy.com/v1/gifs/search?api_key='+apiKey+'&limit=6&q='+trendSearch,
+                method: 'GET'
+            }).then(function(searchObject){
+                relatedGifs = searchObject.data;
+
+                $(relatedGifs).each(function(){
+                    //append related column with gif results
+                    console.log(this.title);
+                })
+
+            });
 
         });
 
